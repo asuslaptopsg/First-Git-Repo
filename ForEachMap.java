dc399266f41b0b3ea34df877a8d2a2fc2130dc84
Amit Patidar <asuslaptopsg@gmail.com>
2022-06-16 13:32:38 +0800

    first commit locally

---
+++ ForEachMap.java
@@ -0,0 +1,22 @@
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class ForEachMap {
+
+    public static void main(String[] args) {
+
+        Map<String, Integer> map = new HashMap<>();
+        map.put("A", 10);
+        map.put("B", 20);
+        map.put("C", 30);
+        map.put("D", 40);
+        map.put("E", 50);
+        map.put("F", 60);
+
+        
+
+    }
+}
+
